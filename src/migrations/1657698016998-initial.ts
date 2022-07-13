d4e119c9ac6f5d2c2c53c9ecfa03b56758ee80ed
Tuğrul Gençoğlu <gencoglutugrul@gmail.com>
2022-07-13 11:40:29 +0300

    created initial migration

---
+++ src/migrations/1657698016998-initial.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Initial1657698016998 implements MigrationInterface {
+  name = 'Initial1657698016998';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "request_entity" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "username" varchar NOT NULL, "loginUrl" varchar NOT NULL, "jobId" integer NOT NULL, CONSTRAINT "REL_64ca6d83afbcf88b662e4f03de" UNIQUE ("jobId"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "job_entity" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "type" varchar(255) NOT NULL, "processedOn" datetime, "finishedOn" datetime, "result" varchar, "isCompleted" boolean NOT NULL DEFAULT (0), "isFailed" boolean NOT NULL DEFAULT (0))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "temporary_request_entity" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "username" varchar NOT NULL, "loginUrl" varchar NOT NULL, "jobId" integer NOT NULL, CONSTRAINT "REL_64ca6d83afbcf88b662e4f03de" UNIQUE ("jobId"), CONSTRAINT "FK_64ca6d83afbcf88b662e4f03de7" FOREIGN KEY ("jobId") REFERENCES "job_entity" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "temporary_request_entity"("id", "username", "loginUrl", "jobId") SELECT "id", "username", "loginUrl", "jobId" FROM "request_entity"`,
+    );
+    await queryRunner.query(`DROP TABLE "request_entity"`);
+    await queryRunner.query(
+      `ALTER TABLE "temporary_request_entity" RENAME TO "request_entity"`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "request_entity" RENAME TO "temporary_request_entity"`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "request_entity" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "username" varchar NOT NULL, "loginUrl" varchar NOT NULL, "jobId" integer NOT NULL, CONSTRAINT "REL_64ca6d83afbcf88b662e4f03de" UNIQUE ("jobId"))`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "request_entity"("id", "username", "loginUrl", "jobId") SELECT "id", "username", "loginUrl", "jobId" FROM "temporary_request_entity"`,
+    );
+    await queryRunner.query(`DROP TABLE "temporary_request_entity"`);
+    await queryRunner.query(`DROP TABLE "job_entity"`);
+    await queryRunner.query(`DROP TABLE "request_entity"`);
+  }
+}
